076a8dce3512b9f0fd55bfd1ebbe7c8affec402e
Heather Nuffer <zzzuzz@gmail.com>
2019-11-08 09:23:11 -0700

    Gets data from API and displays character names

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
+  // Try to think through what state you'll need for this app before starting. 
+  //Then build out
   // the state properties here.
+  const [characters, setCharacters] = useState([]);
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
+  useEffect(() => {
+    axios.get(`https://swapi.co/api/people/?page=1`)
+      .then(response => {
+        console.log(response.data.results);
+        setCharacters(response.data.results);
+      })
+      .catch(error => console.log(error));
+  } ,[]);
+  
+
+
+  // Fetch characters from the star wars api in an effect hook. Remember, 
+  //anytime you have a 
+  // side effect in a component, you want to think about which state and/or 
+  //props it should
   // sync up with, if any.
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      {characters.map((character, index) => {
+        return <p>{character.name}</p>
+      })}
     </div>
   );
 }
