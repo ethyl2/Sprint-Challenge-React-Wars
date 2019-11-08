3d6d13dc5259b97294296b3890479d78d61d39db
Heather Nuffer <zzzuzz@gmail.com>
2019-11-08 10:48:37 -0700

    Stretch: added function to allow the user to display different pages

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,25 +2,40 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
 import CharacterCard from './components/CharacterCard';
-import { Container, Row } from "reactstrap";
+import { Container, Row, Button } from "reactstrap";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. 
   //Then build out
   // the state properties here.
   const [characters, setCharacters] = useState([]);
+  const [page, setPage] = useState('page=1');
+
+  function getCharacters(direction) {
+    const numberPattern = /\d+/g;
+    let num = page.match(numberPattern);
+    console.log(num);
+    (direction === "next")? num++ : num--;
+    
+    if (num > 9) {
+      num = 1;
+    }
+    if (num < 1) {
+      num = 9;
+    }
+    console.log(num);
+    setPage(`page=${num}`);
+  }
 
   useEffect(() => {
-    axios.get(`https://swapi.co/api/people/?page=1`)
+    axios.get(`https://swapi.co/api/people/?${page}`)
       .then(response => {
-        console.log(response.data.results);
+        //console.log(response.data.results);
         setCharacters(response.data.results);
       })
       .catch(error => console.log(error));
-  } ,[]);
+  } ,[page]);
   
-
-
   // Fetch characters from the star wars api in an effect hook. Remember, 
   //anytime you have a 
   // side effect in a component, you want to think about which state and/or 
@@ -30,6 +45,9 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <Button onClick={() => getCharacters("previous")}>Previous Page</Button>
+      <Button className="ml-4" onClick={() => getCharacters("next")}>Next Page</Button>
+      
       <Container>
         <Row>
         {characters.map((character, index) => {
